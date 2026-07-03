9d373357df26cb2d69c00c6fb8c1f48217575900
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-03 23:17:58 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+TRAIN_IMAGES = "data/mnist/train-images.idx3-ubyte"
+TRAIN_LABELS = "data/mnist/train-labels.idx1-ubyte"
+
+TEST_IMAGES = "data/mnist/t10k-images.idx3-ubyte"
+TEST_LABELS = "data/mnist/t10k-labels.idx1-ubyte"
+
+INPUT_SIZE = 784
+HIDDEN_SIZE = 128
+NUM_CLASSES = 10
+
+BATCH_SIZE = 32
+EPOCHS = 10
+
+LEARNING_RATE = 0.01
+
+IMAGE_ROWS = 28
+IMAGE_COLS = 28

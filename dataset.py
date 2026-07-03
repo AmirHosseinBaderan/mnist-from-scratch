855f535d65613c225adea1ea6c767d709a7d1351
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-07-03 18:04:03 +0330

    update : data set add transformer

--- dataset.py
+++ dataset.py
@@ -2,8 +2,19 @@ import struct
 
 
 class MNISTDataset:
+    IMAGE_HEADER_SIZE = 16
+    LABEL_HEADER_SIZE = 8
 
-    def __init__(self, image_path, label_path):
+    def __init__(
+            self,
+            image_path,
+            label_path,
+            image_transform=None,
+            label_transform=None,
+    ):
+        self.image_transform = image_transform
+        self.label_transform = label_transform
+
         self.image_file = open(image_path, "rb")
         self.label_file = open(label_path, "rb")
 
@@ -27,16 +38,35 @@ class MNISTDataset:
         self.label_file.close()
 
     def __getitem__(self, idx):
-        image_offset = 16 + idx * self.rows * self.cols
-        label_offset = 8 + idx
+        self._validate_index(idx)
 
+        image = self._read_image(idx)
+        label = self._read_label(idx)
+
+        if self.image_transform is not None:
+            image = self.image_transform(image)
+        if self.label_transform is not None:
+            label = self.label_transform(label)
+
+        return image, label
+
+    def _validate_index(self,idx):
+        if idx < 0 or idx >= len(self):
+            raise IndexError("Dataset index out of range.")
+
+    def _read_image(self,idx):
+        image_offset = self.IMAGE_HEADER_SIZE + idx * self.rows * self.cols
         self.image_file.seek(image_offset)
         image = self.image_file.read(self.rows * self.cols)
 
+        return image
+
+    def _read_label(self,idx):
+        label_offset = self.LABEL_HEADER_SIZE + idx
         self.label_file.seek(label_offset)
         label = self.label_file.read(1)[0]
 
-        return image, label
+        return label
 
     def _read_image_header(self):
         magic = struct.unpack(">I", self.image_file.read(4))[0]
--- main.py
+++ main.py
@@ -5,19 +5,18 @@ from transformers.compose import Compose
 from transformers.reshape import Reshape
 from transformers.to_numpy import ToNumpy
 
-dataset = MNISTDataset(
-    "data/mnist/train-images.idx3-ubyte",
-    "data/mnist/train-labels.idx1-ubyte",
-)
-
-image, label = dataset[0]
-
 compose = Compose([
     ToNumpy(),
     Reshape(28,28),
 ])
 
-image = compose(image)
+dataset = MNISTDataset(
+    "data/mnist/train-images.idx3-ubyte",
+    "data/mnist/train-labels.idx1-ubyte",
+    image_transform=compose,
+)
+
+image, label = dataset[0]
 
 plt.imshow(image,cmap="gray")
 plt.show()
